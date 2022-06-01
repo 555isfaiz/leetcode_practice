cebd4de3bc81853f4fc756df93c786e43a2fdb45
555isfaiz <1244274837@qq.com>
2022-06-01 10:56:56 +0200

    1480. Running sum of 1d array

---
+++ RunningSumof1dArray.java
@@ -0,0 +1,11 @@
+// 1480
+public class RunningSumof1dArray {
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) res[i] = nums[i];
+            else res[i] = nums[i] + res[i - 1];
+        }
+        return res;
+    }
+}
