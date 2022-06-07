463f828eb633a443b664611a12cb2d4eeaf3c509
555isfaiz <1244274837@qq.com>
2022-06-07 10:04:14 +0200

    88. Merge sorted array

---
+++ MergeSortedArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+// 88
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    // https://leetcode.com/problems/merge-sorted-array/discuss/29704/My-clean-java-solution
+    public void mergeBetter(int[] nums1, int m, int[] nums2, int n) {
+        int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = (nums1[tail1] > nums2[tail2]) ?
+                    nums1[tail1--] : nums2[tail2--];
+        }
+
+        while (tail2 >= 0) { //only need to combine with remaining nums2, if any
+            nums1[finished--] = nums2[tail2--];
+        }
+    }
+}
