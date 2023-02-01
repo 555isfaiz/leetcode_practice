930aadf03f620ce980edc1da0df876d01aa5c898
555isfaiz <1244274837@qq.com>
2023-02-01 10:19:40 +0100

    1071. Greatest Common Divisor of Strings (add better solution)

--- GreatestCommonDivisorofStrings.java
+++ GreatestCommonDivisorofStrings.java
@@ -31,6 +31,20 @@ public class GreatestCommonDivisorofStrings {
         return "";
     }
 
+    // https://leetcode.com/problems/greatest-common-divisor-of-strings/solutions/3124997/super-easy-solution-fully-explained-c-python3-java/
+    public String gcdOfStringsBetter(String str1, String str2) {
+        // Check if concatenated strings are equal or not, if not return ""
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        // If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
     public static void main(String[] args) {
         GreatestCommonDivisorofStrings g = new GreatestCommonDivisorofStrings();
         System.out.println(g.gcdOfStrings("ABCABC", "ABC"));
