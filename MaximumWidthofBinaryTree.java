378a746eadfa8d6e76fb0f8b57cd9c3ac5625305
555isfaiz <1244274837@qq.com>
2023-04-20 13:15:21 +0200

    662. Maximum Width of Binary Tree

---
+++ MaximumWidthofBinaryTree.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumWidthofBinaryTree {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        root.val = 0;
+        q.add(root);
+        int maxWidth = 0;
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int levelStart = q.peek().val;
+            int index = 0;
+
+            for (int i = 0; i < size; i++) {
+                var node = q.poll();
+                index = node.val;
+                
+                if (node.left != null) {
+                    node.left.val = index * 2;
+                    q.add(node.left);
+                }
+                
+                if (node.right != null) {
+                    node.right.val = index * 2 + 1;
+                    q.add(node.right);
+                }
+            }
+            
+            maxWidth = Math.max(maxWidth, index - levelStart + 1);
+        }
+        
+        return maxWidth;
+    }
+
+    public static void main(String[] args) {
+        MaximumWidthofBinaryTree m = new MaximumWidthofBinaryTree();
+        // System.out.println(m.widthOfBinaryTree(TreeNode.getTree(new Object[] {0,0,0,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null})));
+        // System.out.println(m.widthOfBinaryTree(TreeNode.getTree(new Object[] {0,0,0,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null})));
+        System.out.println(m.widthOfBinaryTree(TreeNode.getTree(new Object[] {1,3,2,5})));
+    }
+}
